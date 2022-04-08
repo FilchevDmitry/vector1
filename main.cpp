033e892222179900e5203bd96b11d8fa57a08b25
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-08 15:03:40 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+#include <vector>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+vector<int> num;
+int cou=0;
+cout <<"Enter the length :";
+cin>>cou;
+    for (int i = 0; i <cou ; ++i) {
+        int temp=0;
+        cout<<"Input numbers "<<i+1<<"-";
+        cin>>temp;
+        num.push_back(temp);
+    }
+    for (int i = 0; i <num.size() ; ++i) {
+        cout << num[i] << " ";
+    }
     return 0;
 }
